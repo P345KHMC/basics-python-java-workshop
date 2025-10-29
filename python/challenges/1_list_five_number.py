f1297266ce3c5cc5b78d02b26dc37d04b87bf892
Fatih Ahmad Az Zubair <fsfafageli@gmail.com>
2025-10-29 10:11:49 +0700

    test push

--- python/challenges/1_list_five_number.py
+++ python/challenges/1_list_five_number.py
@@ -3,13 +3,13 @@ numbers = [10, 25, 7, 40, 15]
 print("List awal:", numbers)
 
 # 2. TODO: Tampilkan angka pertama dan terakhir
-print("Angka pertama:")
-print("Angka terakhir:")
+print("Angka pertama:", numbers[0])
+print("Angka terakhir:", numbers[-1:])
 
 # 3. TODO: Tambahkan angka baru (apapun) ke akhir list
-print("List setelah ditambah:", numbers)
+print("List setelah ditambah:", numbers.append(100), numbers)
 
 # 4. TODO: Urutkan list secara descending
 
 # 5. Tampilkan hasil akhir
-print("List urut descending:", numbers)
+print("List urut descending:", numbers[::-1])
